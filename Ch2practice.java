70c9e333dd979db2211858ae27da77ad4baa8989
hxs1rqh <soniahong777@gmail.com>
2026-09-17 15:02:50 -0700

    Add files via upload

---
+++ Ch2practice.java
@@ -0,0 +1,10 @@
+public class Ch2practice {
+	
+	public static void main (String[] args) {
+		
+		String message;
+		message = "My name is Sonia and I am 17 years old";
+		
+		System.out.println(message);
+	}
+}
