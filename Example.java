fd5f2f1340572bf6455f4d1a862c5814e5c2dc95
hxs1rqh <soniahong777@gmail.com>
2026-09-17 14:53:01 -0700

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,12 @@
+public class Example {
+
+	public static void main (String[] args) {
+		
+		String name;
+		name = "Mr P";
+		int x;
+		System.out.println("I said \"Hello, " + name + "!\""); 
+		System.out.println( 7 / 2);
+		
+	}
+}
