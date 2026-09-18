1265d147031b91ed729c21e183aad6176e39c5a5
hxs1rqh <soniahong777@gmail.com>
2026-09-18 14:26:08 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,13 @@
+public class Date {
+	
+	public static void main (String[] args) {
+	
+	int date = 18;
+	int month = 9;
+	int year = 2026; 
+	
+	System.out.println (date + "/" + month + "/" + year);
+	
+}
+}
+
