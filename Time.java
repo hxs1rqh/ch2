44967b542bf594e8f8a78028c0571e479d8d0c9a
hxs1rqh <soniahong777@gmail.com>
2026-09-18 14:43:20 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,12 @@
+public class Time {
+
+public static void main (String[] arg) {
+
+int hour = 14;
+int minutes = 30;
+ int secondsSinceMidnight = (hour * 3600) + (minutes * 60);
+ 
+ System.out.println (secondsSinceMidnight);
+ 
+}
+}
